f9e191d995a753d3279e36081dec0194e3afc862
kylezhao101 <kylezhao101@gmail.com>
2023-07-20 18:50:26 -0700

    Added responsiveness to initial landing and exhibit detail section
    Added custom tailwind config for height and width properties. Renamed js folder to scripts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      backgroundImage:{
+        'hero-landing' : "url('/img/UBCMOA01.png')"
+      },
+      height:{
+        'screen-80' : '80dvh',
+        'screen-70' : '70dvh',
+        'screen-60' : '60dvh',
+        'screen-50' : '50dvh',
+        'screen-40' : '40dvh',
+        'screen-30' : '30dvh',
+        'screen-20' : '20dvh',
+      },
+      width:{
+        'screen-20' : '20vw',
+      },
+      gap:{
+        'screen-w-15' : '15vw'
+      }
     },
   },
   plugins: [],
